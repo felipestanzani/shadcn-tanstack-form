3322166318dc925dd29a5ecd6e073dc80102be5b
Felipe Stanzani <stanzani@gmail.com>
2025-10-04 10:17:47 -0300

    Install new shadcn Field component

--- src/ZodFormExample.tsx
+++ src/ZodFormExample.tsx
@@ -11,7 +11,7 @@ import { Button } from "@/components/ui/button"
 import { Input } from "./components/ui/input"
 import { Checkbox } from "./components/ui/checkbox"
 import { Textarea } from "./components/ui/textarea"
-import { useAppForm } from "./hooks/form-hook"
+import { useAppForm } from "@/hooks/form-hook"
 
 // Define Zod schema
 const userSchema = z.object({
